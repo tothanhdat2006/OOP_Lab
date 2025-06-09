a1f371aea2ad78b1e23a9d1103b50faea7cba55d
ToThanhDat <tothanhdat1421@gmail.com>
2025-06-09 18:15:19 +0700

    Create mainMenu window

---
+++ HomestayManagementSystem/mainMenu.cs
@@ -0,0 +1,15 @@
+namespace HomestayManagementSystem
+{
+    public partial class mainMenu : Form
+    {
+        public mainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
